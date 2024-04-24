18b2acea6c3df08f5d87b430b0110b3e2980cd92
issssaaaas <152148254+issssaaaas@users.noreply.github.com>
2024-04-24 16:08:31 +0000

    Creating Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className='text-red-500 text-5xl'>Hello World!</h1>
-    </div>
-  );
-}
-export default App;
+import './App.css';	import "./App.css";
+import HomePage from "./pages/HomePage";
+
+
+function App() {	function App() {
+  return (	  return (
+    <div className="App">	    <div className="App">
+      <h1 className='text-red-500 text-5xl'>Hello World!</h1>	      <HomePage />
+    </div>	    </div>
+  );	  );
+}	}
+export default App;	export default App;
