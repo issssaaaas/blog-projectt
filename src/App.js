accb318adb0db07d6733f65abbc87d5b6124e5b2
issssaaaas <152148254+issssaaaas@users.noreply.github.com>
2024-04-24 15:50:26 +0000

    setup project 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 classname='text-red-500 text-5xl'>hello world</h1>
+      <h1 className='text-red-500 text-5xl'>Hello World!</h1>
     </div>
   );
 }
-
 export default App;
