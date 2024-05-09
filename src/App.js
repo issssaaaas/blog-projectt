4c5fe5920b1f7bff4092ddf52514485f02eb30d6
issssaaaas <152148254+issssaaaas@users.noreply.github.com>
2024-05-09 23:03:49 +0000

    25 - Profile page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ArticleDetailPage from "./pages/ArticleDetail/ArticleDetailPage";
 import HomePage from "./pages/Home/Homepage";
 import RegisterPage from "./pages/register/RegisterPage";
 import LoginPage from "./pages/login/LoginPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Toaster />
     </div>
